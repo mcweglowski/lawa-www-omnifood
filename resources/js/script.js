3447d3053b65a408e74efb5688b37dec0dd58a5c
Marcin Weglowski <marcin@escali.no>
2020-05-22 13:33:56 +0200

    Remove sections who are not needed in current project

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,23 +1,8 @@
 $(document).ready(function() {
 
-  /*Sticky navigation*/
-  $('.js--section-features').waypoint(function(direction) {
-    if (direction == "down") {
-        $('nav').addClass('sticky')
-    }
-    else {
-        $('nav').removeClass('sticky')
-  }}, {
-    offset: '60px'
-  });
-
   /* Scroll on buttons*/
   $('.js--scroll-to-plans').click(function() {
     $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000)
-  });
-
-  $('.js-scroll-to-start').click(function() {
-    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000)
   });
 
   /* Smooth scrolling */
@@ -60,12 +45,6 @@ $(document).ready(function() {
 
 
   /* Animations on scroll */
-  $('.js--wp-1').waypoint(function(direction) {
-    $('.js--wp-1').addClass('animated fadeIn');
-  }, {
-    offset: '50%'
-  });
-
   $('.js--wp-2').waypoint(function(direction) {
     $('.js--wp-2').addClass('animated fadeInUp');
   }, {
