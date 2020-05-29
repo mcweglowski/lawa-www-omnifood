a5c94bfbe8eba0ca52bc0d3ef98b88dc65c47cd4
Marcin Weglowski <marcin@escali.no>
2020-05-29 15:21:33 +0200

    New logo conept. Nawigation fixes

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   /* Scroll on buttons*/
   $('.js--scroll-to-plans').click(function() {
-    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000)
+    $('html, body').animate({scrollTop: $('.js--section-oferta').offset().top}, 1000)
   });
 
   /* Smooth scrolling */
