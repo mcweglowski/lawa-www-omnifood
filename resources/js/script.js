67b3c4d60e299d256b60ff2ad6f63eca2f9f07cc
Marcin Weglowski <mcweglowski@gmail.com>
2020-07-19 22:24:15 +0200

    Set different speeds for different sections scrolling

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,19 +6,19 @@ $(document).ready(function() {
   });
 
   $('.js--scroll-to-obuwie-ochronne').click(function() {
-    $('html, body').animate({scrollTop: $('.js--section-obuwie-ochronne').offset().top}, 1000)
+    $('html, body').animate({scrollTop: $('.js--section-obuwie-ochronne').offset().top}, 1250)
   });
 
   $('.js--scroll-to-helmy-strazackie').click(function() {
-    $('html, body').animate({scrollTop: $('.js--section-helmy-strazackie').offset().top}, 1000)
+    $('html, body').animate({scrollTop: $('.js--section-helmy-strazackie').offset().top}, 1500)
   });
 
   $('.js--scroll-to-kombinezony-ochronne').click(function() {
-    $('html, body').animate({scrollTop: $('.js--section-kombinezony-ochronne').offset().top}, 1000)
+    $('html, body').animate({scrollTop: $('.js--section-kombinezony-ochronne').offset().top}, 1750)
   });
 
   $('.js--scroll-to-srodki-chronne').click(function() {
-    $('html, body').animate({scrollTop: $('.js--section-srodki-ochronne').offset().top}, 1000)
+    $('html, body').animate({scrollTop: $('.js--section-srodki-ochronne').offset().top}, 2000)
   });
 
   /* Smooth scrolling */
