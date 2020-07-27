2d79e6da36a8b60a647494db060e074f729e5849
Marcin Weglowski <marcin@escali.no>
2020-07-27 14:24:48 +0200

    Notify if some fields in email are not valid

--- resources/js/contactus.js
+++ resources/js/contactus.js
@@ -1,24 +1,44 @@
 function submitToAPI(e) {
 
+    document.getElementById("email-success").style.display = "none";
+    document.getElementById("email-error").style.display = "none";
+    document.getElementById("name-input").style.borderColor = "#CCCCC";
+    document.getElementById("name-input-empty").style.display = "none";
+    document.getElementById("email-input").style.borderColor = "#CCCCC";
+    document.getElementById("email-input-empty").style.display = "none";
+    document.getElementById("description-input").style.borderColor = "#CCCCC";
+    document.getElementById("description-input-empty").style.display = "none";
+
+
     e.preventDefault();
     var URL = "https://ymyzm6gw71.execute-api.eu-west-1.amazonaws.com/LawaProd/contact-us";
 
+    var valid = true;
+
     var Namere = /[A-Za-z]{1}[A-Za-z]/;
-         if (!Namere.test($("#name-input").val())) {
-                      alert ("Name can not less than 2 char");
-             return;
-         }
-         if ($("#email-input").val()=="") {
-             alert ("Please enter your email id");
-             return;
-         }
+         
+    if (!Namere.test($("#name-input").val())) {
+      document.getElementById("name-input").style.borderColor = "red";
+      document.getElementById("name-input-empty").style.display = "block";
+      valid = false;
+    }
+    
+    if ($("#email-input").val()=="") {
+      document.getElementById("email-input").style.borderColor = "red";
+      document.getElementById("email-input-empty").style.display = "block";
+      valid = false;
+    }
+         
+    if ($("#description-input").val()=="") {
+      document.getElementById("description-input").style.borderColor = "red";
+      document.getElementById("description-input-empty").style.display = "block";
+      valid = false;
+    }
 
-         var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
-         if (!reeamil.test($("#email-input").val())) {
-             alert ("Please enter valid email address");
-             return;
-         }
-
+    if (false == valid) {
+      return;
+    }
+ 
     var name = $("#name-input").val();
     var email = $("#email-input").val();
     var desc = $("#description-input").val();
@@ -37,6 +57,7 @@ function submitToAPI(e) {
       data: JSON.stringify(data),
 
       success: function () {
+        document.getElementById("contact-form").reset();        
         document.getElementById("email-success").style.display = "block";
       },
       error: function () {
